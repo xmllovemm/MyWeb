25bb17c0e38085397f41da7ba03ff58af7758aa0
xiemaolin <xiemaolin@126.com>
2015-07-22 16:37:40 +0800

    add index.jsp

---
+++ src/util/SayHello.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.http.HTTPException;
+
+public class SayHello extends HttpServlet {
+	
+	public void service(HttpServletRequest request, HttpServletResponse response) throws HTTPException,IOException {
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("Hello,welcome to my web!");
+	}
+}
